93d6ec60f9e999b960e4fb63c1ef01b5ab6ff6ec
Christopher Guess <cguess@gmail.com>
2018-02-14 15:37:14 -0600

    trying to use pty for input

--- push.rb
+++ push.rb
@@ -780,7 +780,7 @@ def generateIOS options, version_number = "1.0", build_number = "1"
 						writer.puts code
 					end
 
-					reader.expect "Password (for" do
+					reader.expect /(Please provide your Apple Developer Program).+/ do
 						password = ask "Password: "
 						writer.puts password
 					end
